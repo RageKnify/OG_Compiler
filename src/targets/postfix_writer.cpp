f383bae4bc16e57fa92dac97cefe0d9c184d6dd0
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2020-03-16 19:05:01 +0000

    Implement address_of_node

--- src/targets/postfix_writer.cpp
+++ src/targets/postfix_writer.cpp
@@ -270,3 +270,8 @@ void og::postfix_writer::do_if_else_node(og::if_else_node * const node, int lvl)
   node->elseblock()->accept(this, lvl + 2);
   _pf.LABEL(mklbl(lbl1 = lbl2));
 }
+
+//---------------------------------------------------------------------------
+
+void og::postfix_writer::do_address_of_node(og::address_of_node * const node, int lvl) {
+}
