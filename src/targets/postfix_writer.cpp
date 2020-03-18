b67cf0f1ae11107add34fa0272330936f59dd3ec
Marcelo Santos <marcelocmsantos@gmail.com>
2020-03-18 15:05:52 +0000

    Implement function declaration node

--- src/targets/postfix_writer.cpp
+++ src/targets/postfix_writer.cpp
@@ -270,3 +270,6 @@ void og::postfix_writer::do_if_else_node(og::if_else_node * const node, int lvl)
   node->elseblock()->accept(this, lvl + 2);
   _pf.LABEL(mklbl(lbl1 = lbl2));
 }
+
+void og::postfix_writer::do_function_declaration_node(og::function_declaration_node *const node, int lvl) {
+}
