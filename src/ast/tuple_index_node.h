af31a53ca0faf9aeb248768c706a374029daa2c7
Marcelo Santos <marcelocmsantos@gmail.com>
2020-04-06 12:51:34 +0100

    Take expression node in tuple index
    Because it could either be an rvalue_node or a tuple_node

--- src/ast/tuple_index_node.h
+++ src/ast/tuple_index_node.h
@@ -1,23 +1,23 @@
 #ifndef __OG_AST_TUPLE_INDEX_NODE_H__
 #define __OG_AST_TUPLE_INDEX_NODE_H__
 
+#include <cdk/ast/expression_node.h>
 #include <cdk/ast/integer_node.h>
 #include <cdk/ast/lvalue_node.h>
-#include "tuple_node.h"
 
 namespace og {
 
 	class tuple_index_node: public cdk::lvalue_node {
-		og::tuple_node* _tuple;
+		cdk::expression_node* _tuple;
 		cdk::integer_node* _index;
 
 	public:
-		inline tuple_index_node(int lineno, og::tuple_node* tuple, cdk::integer_node* index) :
+		inline tuple_index_node(int lineno, cdk::expression_node* tuple, cdk::integer_node* index) :
 			cdk::lvalue_node(lineno), _tuple(tuple), _index(index) {
 		}
 
 	public:
-		inline og::tuple_node* tuple() {
+		inline cdk::expression_node* tuple() {
 			return _tuple;
 		}
 
