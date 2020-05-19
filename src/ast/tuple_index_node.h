09a540d4f8bc6e420a696090e9c534f71efd58d8
João Borges <joao.p.l.borges@tecnico.ulisboa.pt>
2020-05-19 22:58:40 +0100

    tuple_index: change left-part from expr to lval

--- src/ast/tuple_index_node.h
+++ src/ast/tuple_index_node.h
@@ -1,22 +1,21 @@
 #ifndef __OG_AST_TUPLE_INDEX_NODE_H__
 #define __OG_AST_TUPLE_INDEX_NODE_H__
 
-#include <cdk/ast/expression_node.h>
 #include <cdk/ast/lvalue_node.h>
 
 namespace og {
 
 	class tuple_index_node: public cdk::lvalue_node {
-		cdk::expression_node* _tuple;
+		cdk::lvalue_node* _tuple;
 		int _index;
 
 	public:
-		inline tuple_index_node(int lineno, cdk::expression_node* tuple, int index) :
+		inline tuple_index_node(int lineno, cdk::lvalue_node* tuple, int index) :
 			cdk::lvalue_node(lineno), _tuple(tuple), _index(index) {
 		}
 
 	public:
-		inline cdk::expression_node* tuple() {
+		inline cdk::lvalue_node* tuple() {
 			return _tuple;
 		}
 
