e84112387a922d2a568a50dd8520fe5e6f125d2f
Marcelo Santos <marcelocmsantos@gmail.com>
2020-05-22 03:51:20 +0100

    Implement upper half of frame size calculator methods

--- src/targets/frame_size_calculator.cpp
+++ src/targets/frame_size_calculator.cpp
@@ -24,73 +24,87 @@ void og::frame_size_calculator::do_string_node(cdk::string_node * const node, in
 	// EMPTY
 }
 void og::frame_size_calculator::do_not_node(cdk::not_node * const node, int lvl) {
-	// EMPTY
+	node->argument()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_and_node(cdk::and_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_or_node(cdk::or_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_identity_node(og::identity_node * const node, int lvl) {
-	// EMPTY
+	node->argument()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_neg_node(cdk::neg_node * const node, int lvl) {
-	// EMPTY
+	node->argument()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_add_node(cdk::add_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_sub_node(cdk::sub_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_mul_node(cdk::mul_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_div_node(cdk::div_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_mod_node(cdk::mod_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_lt_node(cdk::lt_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_le_node(cdk::le_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_ge_node(cdk::ge_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_gt_node(cdk::gt_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_ne_node(cdk::ne_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_eq_node(cdk::eq_node * const node, int lvl) {
-	// EMPTY
+	node->left()->accept(this, lvl + 2);
+	node->right()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_variable_node(cdk::variable_node * const node, int lvl) {
 	// EMPTY
 }
 void og::frame_size_calculator::do_rvalue_node(cdk::rvalue_node * const node, int lvl) {
-	// EMPTY
+	node->lvalue()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_assignment_node(cdk::assignment_node * const node, int lvl) {
-	// EMPTY
+	node->lvalue()->accept(this, lvl + 2);
+	node->rvalue()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_evaluation_node(og::evaluation_node * const node, int lvl) {
-	// EMPTY
+	node->argument()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_print_node(og::print_node * const node, int lvl) {
-	// EMPTY
+	node->arguments()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_read_node(og::read_node * const node, int lvl) {
 	// EMPTY
 }
 void og::frame_size_calculator::do_sizeof_node(og::sizeof_node * const node, int lvl) {
-	// EMPTY
+	node->tuple()->accept(this, lvl + 2);
 }
 void og::frame_size_calculator::do_memory_reservation_node(og::memory_reservation_node * const node, int lvl) {
 	// EMPTY
